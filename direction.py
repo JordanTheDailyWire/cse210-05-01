2a11ada128c25f82abbb30ad02908dfdf9338e66
Fireguy13 <104537058+Fireguy13@users.noreply.github.com>
2022-06-22 20:32:34 -0600

    update

--- direction.py
+++ direction.py
@@ -1,52 +1,18 @@
-import constants
-from execution import Executes
-from game.shared.point import Point
+class Direction():
 
+    valid_directions = [(-1,0), (0,-1), (+1,0), (0,+1)]
+    _direction = None
 
-class Direction(Executes):
-   
+    def __init__(self, initial_direction):
+        self.set_direction(initial_direction)
 
-    def __init__(self, keyboard_service):
-        
-        self._keyboard_service = keyboard_service
-        self._direction = Point(constants.CELL_SIZE, 0)
-        self._direction2 = Point(constants.CELL_SIZE, 0)
+    def validate_direction(self, dir):
+        if dir not in self.valid_directions:
+            raise Exception("Invalid direction")
 
-    def execute(self, cast, script):
-        
-        # left
-        if self._keyboard_service.is_key_down('a'):
-            self._direction = Point(-constants.CELL_SIZE, 0)
-        
-        # right
-        if self._keyboard_service.is_key_down('d'):
-            self._direction = Point(constants.CELL_SIZE, 0)
-        
-        # up
-        if self._keyboard_service.is_key_down('w'):
-            self._direction = Point(0, -constants.CELL_SIZE)
-        
-        # down
-        if self._keyboard_service.is_key_down('s'):
-            self._direction = Point(0, constants.CELL_SIZE)
+    def set_direction(self, direction):
+        self.validate_direction(direction)
+        self._direction = direction
 
-        # left 
-        if self._keyboard_service.is_key_down('i'):
-            self._direction2 = Point(-constants.CELL_SIZE, 0)
-        
-        # right
-        if self._keyboard_service.is_key_down('j'):
-            self._direction2 = Point(constants.CELL_SIZE, 0)
-        
-        # up
-        if self._keyboard_service.is_key_down('k'):
-            self._direction2 = Point(0, -constants.CELL_SIZE)
-        
-        # down
-        if self._keyboard_service.is_key_down('l'):
-            self._direction2 = Point(0, constants.CELL_SIZE)
-        
-        player1 = cast.get_first_actor("player1")
-        player2 = cast.get_first_actor("player2")
-        player1.turn_head(self._direction)
-        player2.turn_head(self._direction)
+    def get_direction(self):
+        return self._direction
--- execution.py
+++ execution.py
@@ -1,4 +1,24 @@
-class Executes:
-   
-    def execute(self, players, script):
-        pass
+from keyboard_service import KeyboardService
+class Execution():
+    #direction is used to apply a move for the player it returns the score so
+    # that the script can be display:
+    def __init__(self) -> None:
+        super().__init__()
+        self.new_execute = (self.ex, self.ey)
+        self.ex = (hx + x)
+        self.ey = (hy + y)
+        self.location = self.get_head(hx = 0, hy =0)
+        self.coordinates = self.point(x = 0, y = 0)
+        
+    def execution(self, players, snake):
+        """Executes something that is important in the game. This method should be overriden by
+        derived classes.
+        Args:
+            self
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game."""
+        for self.new_execution = self.location + self.coordinate
+            self.ex = hx + x
+            self.ey = hy + y
+            self.new_execution == self.execution
+            return (self.ex, self.ey)
