f2b721cfa416ad7274d07cd63c31632564c45feb
LaceBowr <97077997+LaceBowr@users.noreply.github.com>
2022-06-24 16:47:29 -0700

    updated 6/24/22

--- snake.py
+++ snake.py
@@ -1,6 +1,9 @@
 import constants
 from point import Point
-class Snake():
+from contestants import Contestants
+import constants
+
+class Snake(Contestants):
     """
     A long limbless reptile.
     The responsibility of Snake is to move itself.
@@ -10,15 +13,18 @@ class Snake():
     color = constants.WHITE
     _segments = []
     _direction = None
+    
     def __init__(self, initial_segment_list):
-        # initial segment list is a list of points that represents
+        # initial segment list is a list of points that represents 
         # the initial body segment positions
-        super().__init__()
         self._segments = initial_segment_list
+    
     def set_direction(self, direction):
         self._direction = direction
+
     def get_segments(self):
         return self._segments
+
     def move_next(self):
         # move all segments
         for segment in self._segments:
@@ -31,6 +37,7 @@ class Snake():
             trailing.set_velocity(velocity)
     def get_head(self):
         return self._segments[0]
+    
     def grow_tail(self, number_of_segments):
         for i in range(number_of_segments):
             tail = self._segments[-1]
@@ -43,5 +50,7 @@ class Snake():
             segment.set_text("#")
             segment.set_color(constants.GREEN)
             self._segments.append(segment)
+    
     def turn_head(self, velocity):
         self._segments[0].set_velocity(velocity)
+    
